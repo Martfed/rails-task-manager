1a6272ab68acf016f182cb2726e3e0babeb5bbcc
Federico De Martino <federicodm90@gmail.com>
2019-08-06 14:02:39 +0200

    exercise 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,5 @@
 Rails.application.routes.draw do
-  # resources :tasks, as: :edit_task, path: 'tasks/:id/edit', as: :task, path: 'tasks/:id'
+  resources :tasks
   # list all tasks
-  get 'tasks', to: 'tasks#index'
 
-  # create new task
-  get 'tasks/new', to: 'tasks#new'
-  post 'tasks', to: 'tasks#create'
-
-  # list one task
-  get 'tasks/:id', to: 'tasks#show', as: :task
-
-  # update a task
-  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
-  patch 'tasks/:id', to: 'tasks#update', as: :delete_task
-
-  # delete a task
-  delete 'tasks/:id', to: 'tasks#destroy'
 end
